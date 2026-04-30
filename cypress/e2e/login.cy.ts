ce4f58abfde540e71171e924bad31690f5e22c02
LawalH-DCoder <hamzat.lawal@interswitchng.com>
2026-04-30 11:24:05 +0100

    fix: cypress err

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -14,7 +14,7 @@ describe('Login', () => {
   });
   it('shows error message for wrong credentials', () => {
     cy.intercept('GET', '**/users?email=*', { body: [] }); // no match
-    cy.get('[data-testid="login-email-input"]').type('wrong@email.com');
+    cy.get('[data-testid="login-email-input"]').type('wrong@gmail.com');
     cy.get('[data-testid="login-password-input"]').type('wrongpass');
     cy.get('[data-testid="login-submit-button"]').click();
     cy.get('[data-testid="login-error"]').should('be.visible');
